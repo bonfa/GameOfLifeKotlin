5a7b78c42489da5b57f3c0d48f8dbfbcfecfe6d6
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2017-07-08 18:23:30 +0200

    clean a bit the game of life rule

--- src/main/kotlin/it/bonfadelli/game/of/life/GameOfLifeRule.kt
+++ src/main/kotlin/it/bonfadelli/game/of/life/GameOfLifeRule.kt
@@ -1,24 +1,33 @@
 package it.bonfadelli.game.of.life
 
+import it.bonfadelli.game.of.life.Cell.State.ALIVE
+import it.bonfadelli.game.of.life.Cell.State.DEAD
+
 class GameOfLifeRule {
 
     fun evolve(matrix: List<List<Cell>>): List<List<Cell>> {
         val evolutionMatrix: MutableList<MutableList<Cell>> = ArrayList()
         for (y in 0..matrix.size - 1) {
+            evolutionMatrix.add(ArrayList<Cell>())
             val row = matrix[y]
             val minY = maxOf(0, y - 1)
-            val maxY = minOf(matrix.count() - 1, y + 1)
-            evolutionMatrix.add(ArrayList<Cell>())
-            for (x in 0..row.count() - 1) {
+            val maxY = minOf(matrix.size - 1, y + 1)
+            for (x in 0..row.size - 1) {
                 val minX = maxOf(0, x - 1)
-                val maxX = minOf(row.count() - 1, x + 1)
-                appendEvolvedStatus(x, y, matrix, evolutionMatrix, minX, minY, maxX, maxY)
+                val maxX = minOf(row.size - 1, x + 1)
+                val evolvedCell = getEvolvedCell(matrix, x, y, minX, minY, maxX, maxY)
+                evolutionMatrix[y].add(evolvedCell)
             }
         }
         return evolutionMatrix
     }
 
-    private fun appendEvolvedStatus(xCell: Int, yCell: Int, original: List<List<Cell>>, evolved: MutableList<MutableList<Cell>>, minX: Int, minY: Int, maxX: Int, maxY: Int) {
+    private fun getEvolvedCell(startingCells: List<List<Cell>>, xCell: Int, yCell: Int, minX: Int, minY: Int, maxX: Int, maxY: Int): Cell {
+        val numberOfAliveSiblings = getNumberOfAliveSiblings(startingCells, xCell, yCell, minX, minY, maxX, maxY)
+        return getEvolvedCell(numberOfAliveSiblings, startingCells[yCell][xCell])
+    }
+
+    private fun getNumberOfAliveSiblings(original: List<List<Cell>>, xCell: Int, yCell: Int, minX: Int, minY: Int, maxX: Int, maxY: Int): Int {
         var numberOfAliveSiblings = 0
         for (y in minY..maxY) {
             for (x in minX..maxX) {
@@ -29,15 +38,13 @@ class GameOfLifeRule {
                 }
             }
         }
-
-        val newCell = getNewCellState(numberOfAliveSiblings, original[yCell][xCell])
-        evolved[yCell].add(newCell)
+        return numberOfAliveSiblings
     }
 
-    private fun getNewCellState(numberOfAliveSiblings: Int, cell: Cell): Cell {
-        var newCell = Cell(Cell.State.DEAD)
-        if (cell.isAlive() && numberOfAliveSiblings == 2) {
-            newCell = Cell(Cell.State.ALIVE)
+    private fun getEvolvedCell(numberOfAliveSiblings: Int, oldCell: Cell): Cell {
+        var newCell = Cell(DEAD)
+        if (oldCell.isAlive() && numberOfAliveSiblings == 2) {
+            newCell = Cell(ALIVE)
         }
         return newCell
     }
