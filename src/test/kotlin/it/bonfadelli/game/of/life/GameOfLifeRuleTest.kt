747da69ae5d56325e1dc86451496f4700549cfc3
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2017-07-08 16:09:13 +0200

    now the test about the business rule are made inside the GameOfLifeRule test

--- src/test/kotlin/it/bonfadelli/game/of/life/GameOfLifeRuleTest.kt
+++ src/test/kotlin/it/bonfadelli/game/of/life/GameOfLifeRuleTest.kt
@@ -1,5 +1,55 @@
 package it.bonfadelli.game.of.life
 
-/**
- * Created by fbonfadelli on 08/07/2017.
- */
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.BehaviorSpec
+import it.bonfadelli.game.of.life.Cell.State.*
+
+class GameOfLifeRuleTest : BehaviorSpec() {
+    init {
+        given("a single dead cell") {
+            val game: GameOfLifeRule = GameOfLifeRule()
+            `when`("the cell evolves") {
+                val evolution = game.evolve(arrayOf(Cell(DEAD)))
+                then("it is still dead") {
+                    evolution shouldBe arrayOf(Cell(DEAD))
+                }
+            }
+        }
+
+        given("two dead cells") {
+            val game: GameOfLifeRule = GameOfLifeRule()
+            `when`("the cells evolve") {
+                val evolution = game.evolve(arrayOf(Cell(DEAD), Cell(DEAD)))
+                then("they are both dead") {
+                    evolution shouldBe arrayOf(Cell(DEAD), Cell(DEAD))
+                }
+            }
+        }
+
+        given("three alive cells") {
+            val game: GameOfLifeRule = GameOfLifeRule()
+            `when`("the cells evolve") {
+                val evolution = game.evolve(arrayOf(Cell(ALIVE), Cell(ALIVE), Cell(ALIVE)))
+                then("the central cell is alive") {
+                    evolution shouldBe arrayOf(Cell(DEAD), Cell(ALIVE), Cell(DEAD))
+                }
+            }
+        }
+
+        given("two alive and one dead cells") {
+            val game: GameOfLifeRule = GameOfLifeRule()
+            val anotherGame: GameOfLifeRule = GameOfLifeRule()
+            val yetAnotherGame: GameOfLifeRule = GameOfLifeRule()
+            `when`("the cells evolve") {
+                val evolution = game.evolve(arrayOf(Cell(ALIVE), Cell(ALIVE), Cell(DEAD)))
+                val anotherEvolution = anotherGame.evolve(arrayOf(Cell(DEAD), Cell(ALIVE), Cell(ALIVE)))
+                val yetAnotherEvolution = yetAnotherGame.evolve(arrayOf(Cell(ALIVE), Cell(DEAD), Cell(ALIVE)))
+                then("all the cells are dead") {
+                    evolution shouldBe arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD))
+                    anotherEvolution shouldBe arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD))
+                    yetAnotherEvolution shouldBe arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD))
+                }
+            }
+        }
+    }
+}
